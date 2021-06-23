47e8c3126b99ed327945b98a086ba5e070ce0c5e
dhananjoycd <86339031+dhananjoycd@users.noreply.github.com>
2021-06-23 11:51:24 +0600

    Add files via upload

---
+++ FinalResult17072020marksdd.cpp
@@ -0,0 +1,212 @@
+#include <conio.h>
+#include <stdio.h>
+int main(){
+    float A,c,b,ba,e,i,m,p,R,pg,cg,mg,bg,bag,eg,ig,Ag,S;
+     int l;
+    {
+    printf("\nEnter Bangla marks\":"); 
+    scanf("%f",&ba);  
+    if(ba<=32) printf("You Have failed😥");
+    else if ((ba>=33)&&(ba<40)) printf("You have got D = GPA-1.00");
+    else if ((ba>=40)&&(ba<50))printf("You have got C = GPA-2.00");
+    else if ((ba>=50)&&(ba<60)) printf("You have got B = GPA-3.00");
+    else if ((ba>=60)&&(ba<70)) printf("You have got A- = GPA-3.50"); 
+     else if ((ba>=70)&&(ba<80)) printf("You have got A = GPA-4.00"); 
+   else printf("You have got A+ = GPA-5.00 "); 
+   
+}{
+    printf("\n\vEnter English marks\":"); 
+    scanf("%f",&e);  
+    if(e<=32) printf("You Have failed😥");
+    else if ((e>=33)&&(e<40)) printf("You have got D = GPA-1.00");
+    else if ((e>=40)&&(e<50))printf("You have got C = GPA-2.00");
+    else if ((e>=50)&&(e<60)) printf("You have got B = GPA-3.00");
+    else if ((e>=60)&&(e<70)) printf("You have got A- = GPA-3.50"); 
+     else if ((e>=70)&&(e<80)) printf("You have got A = GPA-4.00"); 
+   else printf("You have got A+ = GPA-5.00 "); 
+   
+}
+{
+    printf("\n\vEnter ICT marks\":"); 
+    scanf("%f",&i);  
+    if(i<=32) printf("You Have failed😥");
+    else if ((i>=33)&&(i<40)) printf("You have got D = GPA-1.00");
+    else if ((i>=40)&&(i<50))printf("You have got C = GPA-2.00");
+    else if ((i>=50)&&(i<60)) printf("You have got B = GPA-3.00");
+    else if ((i>=60)&&(i<70)) printf("You have got A- = GPA-3.50"); 
+     else if ((i>=70)&&(i<80)) printf("You have got A = GPA-4.00"); 
+   else printf("You have got A+ = GPA-5.00 "); 
+   
+}
+{
+    printf("\n\vEnter physics marks\":"); 
+    scanf("%f",&p);  
+    if(p<=32) printf("You Have failed😥");
+    else if ((p>=33)&&(p<40)) printf("You have got D = GPA-1.00");
+    else if ((p>=40)&&(p<50))printf("You have got C = GPA-2.00");
+    else if ((p>=50)&&(p<60)) printf("You have got B = GPA-3.00");
+    else if ((p>=60)&&(p<70)) printf("You have got A- = GPA-3.50"); 
+     else if ((p>=70)&&(p<80)) printf("You have got A = GPA-4.00"); 
+   else printf("You have got A+ = GPA-5.00 "); 
+   
+}
+    {
+    printf("\n\vEnter Chemistry marks\":"); 
+    scanf("%f",&c);  
+    if(c<=32) printf("You Have failed😥");
+    else if ((c>=33)&&(c<40)) printf("You have got D = GPA-1.00");
+    else if ((c>=40)&&(c<50))printf("You have got C = GPA-2.00");
+    else if ((c>=50)&&(c<60)) printf("You have got B = GPA-3.00");
+    else if ((c>=60)&&(c<70)) printf("You have got A- = GPA-3.50"); 
+     else if ((c>=70)&&(c<80)) printf("You have got A = GPA-4.00"); 
+   else printf("You have got A+ = GPA-5.00 "); 
+   
+}
+    {
+    printf("\n\vEnter Math marks\":"); 
+    scanf("%f",&m);  
+    if(m<=32) printf("You Have failed😥");
+    else if ((m>=33)&&(m<40)) printf("You have got D = GPA-1.00");
+    else if ((m>=40)&&(m<50))printf("You have got C = GPA-2.00");
+    else if ((m>=50)&&(m<60)) printf("You have got B = GPA-3.00");
+    else if ((m>=60)&&(m<70)) printf("You have got A- = GPA-3.50"); 
+     else if ((m>=70)&&(m<80)) printf("You have got A = GPA-4.00"); 
+   else printf("You have got A+ = GPA-5.00 "); 
+   
+}
+    {
+    printf("\n\vEnter Biology marks\":"); 
+    scanf("%f",&b);  
+    if(b<=32) printf("You Have failed😥");
+    else if ((b>=33)&&(b<40)) printf("You have got D = GPA-1.00");
+    else if ((b>=40)&&(b<50))printf("You have got C = GPA-2.00");
+    else if ((b>=50)&&(b<60)) printf("You have got B = GPA-3.00");
+    else if ((b>=60)&&(b<70)) printf("You have got A- = GPA-3.50"); 
+     else if ((b>=70)&&(b<80)) printf("You have got A = GPA-4.00"); 
+   else printf("You have got A+ = GPA-5.00 "); 
+   
+}
+    {
+            printf("\n\v\a-------Notice at Top -----");
+            printf("\n---Note Your Each subject GPA---\v");
+            
+        }
+        
+        
+   {
+    printf("যদি আপনার অপশনাল গণিত হয় তাহলে-\"1\" অথবা, জীববিজ্ঞান হয় তাহলে-\"2\" ইনপুট দিনঃ");
+ 
+{ printf("\n\vগনিত(১)/জীববিজ্ঞান(২) নির্বাচন করুনঃ\t"); 
+    scanf("%d",&l);
+    
+    
+        if (l==1){
+     printf("\n\v অপশনাল বিষয় \"গণিত\" বাচাই করা সম্পন্ন হয়েছে😍😍😍\n\f সকল বিষয়ের GPA নম্বর ইনপুট করুন।অর্থাৎ\n\v");
+            printf("\n\v বাংলা GPA:\t");
+            scanf("%f",&bag);
+           
+             printf("\n\v ইংরেজী GPA:\t");
+            scanf("%f",&eg);
+           
+             printf("\n\v আইসিটি GPA:\t");
+            scanf("%f",&ig);
+          
+              printf("\n\v পদার্থ GPA:\t");
+            scanf("%f",&pg);
+            
+            printf("\n\v রসায়ন GPA:\t");
+            scanf("%f",&cg);
+            
+            printf("\n\v জীববিজ্ঞান GPA:\t");
+            scanf("%f",&bg);
+            
+            printf("\n\v গণিত GPA:\t");
+            scanf("%f",&mg);
+            if (mg>2)  
+                { printf("\n\v মোটঃ %.2f",S=(pg+cg+bg+bag+eg+ig+mg-2)); }
+            else { printf("\n\v মোটঃ %.2f",S=(pg+cg+bg+bag+eg+ig)); }
+            
+            Ag=(S/6);
+            
+           
+        if ((pg>5)||(bag>5)||(eg>5)||(ig>5)||(cg>5)||(bg>5)||(mg>5))
+           { printf("\n\v ohoo! কোন বিষয়ে GPA-5.00 এর বেশি মান ইনপুট হয়েছে যার দরুণ GPA:- %.2f গ্রহণ যোগ্য নহে। আবার চেষ্টা করুন",Ag);}
+                
+                
+                
+    else {  if (Ag>5)
+           { printf("\n\v Your GPA is:5.00");}
+        
+                
+           else if((pg&&cg&&bg&&bag&&eg&&ig)!=0)
+           { printf("\n\v Your GPA is:%.2f",Ag);}   
+                
+                
+                
+                
+              
+            
+             else {printf("\n\v Ohoo! তুমি ফেল করেছে 😥😥😥");}          
+                
+                
+                
+                
+   }   
+            
+     
+             
+            
+  }    
+   else if(l==2) {printf("\n\v অপশনাল বিষয় \"জীববিজ্ঞান\" বাচাই করা সম্পন্ন হয়েছে😍😍😍\n\f সকল বিষয়ের GPA নম্বর ইনপুট করুন।অর্থাৎ\n\v");
+            
+             printf("\n\v বাংলা GPA:\t");
+            scanf("%f",&bag);
+           
+             printf("\n\v ইংরেজী GPA:\t");
+            scanf("%f",&eg);
+           
+             printf("\n\v আইসিটি GPA:\t");
+            scanf("%f",&ig);
+          
+              printf("\n\v পদার্থ GPA:\t");
+            scanf("%f",&pg);
+            
+            printf("\n\v রসায়ন GPA:\t");
+            scanf("%f",&cg);
+            
+            printf("\n\v জীববিজ্ঞান GPA:\t");
+            scanf("%f",&bg);
+            
+            printf("\n\v গণিত GPA:\t");
+            scanf("%f",&mg);
+            if (bg>2)  
+                { printf("\n\v মোটঃ %.2f",S=(pg+cg+bg+bag+eg+ig+mg-2)); }
+            else { printf("\n\v মোটঃ %.2f",S=(pg+cg+mg+bag+eg+ig)); }
+            
+            Ag=(S/6);
+            
+           
+        if ((pg>5)||(bag>5)||(eg>5)||(ig>5)||(cg>5)||(bg>5)||(mg>5))
+           { printf("\n\v ohoo! কোন বিষয়ে GPA-5.00 এর বেশি মান ইনপুট হয়েছে যার দরুণ GPA:- %.2f গ্রহণ যোগ্য নহে। আবার চেষ্টা করুন",Ag);}
+                
+                
+                
+    else {  if (Ag>5)
+           { printf("\n\v Your GPA is:5.00");}
+        
+                
+           else if((pg&&cg&&bg&&bag&&eg&&ig)!=0)
+           { printf("\n\v Your GPA is:%.2f",Ag);}   
+                
+                
+                
+                
+              
+            
+             else {printf("\n\v Ohoo! তুমি ফেল করেছে 😥😥😥");}}}
+           
+        else {printf("\n\v Ohoo! 🙄🙄🙄 ভুল সংখ্যা ইনপুট হয়েছে। আবার চেষ্টা করুন");}   
+            
+ }  }   
+ getch();
+}
